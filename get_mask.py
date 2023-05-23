a4bf3f3256ca45d7590a7640365a28d0a27f1148
Jane Doe <haitianh@2073L-207327-M.local>
2023-05-23 21:51:33 +1000

    update

--- get_mask.py
+++ get_mask.py
@@ -5,6 +5,7 @@ from numpy import asarray
 
 sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
+# TODO: more options to be adjusted: pred_iou_thresh,
 
 image = Image.open("./datasets/loco/breakfast_box/train/good/000.png")
 image_np = asarray(image)
