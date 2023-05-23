efa85211d45a0ef01aafe30a4b4bce2bc7723b91
Jane Doe <haitianh@2073L-207327-M.local>
2023-05-23 20:11:54 +1000

    update

--- get_mask.py
+++ get_mask.py
@@ -1,6 +1,13 @@
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from PIL import Image
+from numpy import asarray
+
 
 sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
 mask_generator = SamAutomaticMaskGenerator(sam)
-masks = mask_generator.generate("./datasets/loco/breakfast_box/train/good/000.png")
+
+image = Image.open("./datasets/loco/breakfast_box/train/good/000.png")
+image_np = asarray(image)
+
+masks = mask_generator.generate(image_np)
 print(masks)
