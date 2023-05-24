9b0426923625128f915dab0fafdeff2f2c1341fe
Jane Doe <haitianh@2073L-207327-M.local>
2023-05-24 17:51:31 +1000

    update

--- get_mask.py
+++ get_mask.py
@@ -2,37 +2,71 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 from PIL import Image
 from numpy import asarray
 import numpy as np
+import time
+import json
+import random, os
+from datetime import datetime
 
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+output_folder = f"output_{timestamp}"
 
 sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
-mask_generator = SamAutomaticMaskGenerator(sam)
-# TODO: more options to be adjusted: pred_iou_thresh,
+mask_generator = SamAutomaticMaskGenerator(
+    sam
+)  # TODO: more options to be adjusted: pred_iou_thresh,
 
-image = Image.open("./datasets/loco/breakfast_box/train/good/000.png")
-image_np = asarray(image)
 
-masks = mask_generator.generate(image_np)
+categories = [
+    "breakfast_box",
+    "juice_bottle",
+    "pushpins",
+    "screw_bag",
+    "splicing_connectors",
+]
 
-# overlay masks to the image
-if len(masks) > 0:
-    sorted_anns = sorted(masks, key=(lambda x: x["area"]), reverse=True)
-    image.convert("RGBA")
-    width = sorted_anns[0]["segmentation"].shape[0]
-    height = sorted_anns[0]["segmentation"].shape[1]
+os.makedirs(output_folder)
 
-    for ann in sorted_anns:
-        overlay = np.ones(
-            (
-                width,
-                height,
-                4,
+for category in categories:
+    image = Image.open(f"./datasets/loco/{category}/train/good/000.png")
+    image_np = asarray(image)
+    masks = mask_generator.generate(image_np)
+
+    # with open("seg_data.npy", "wb") as f:
+    #     np.save(f, segmentation_results)
+
+    # with open("seg_data.npy", "rb") as f:
+    #     segmentation_results = np.load(f)
+
+    if len(masks) > 0:
+        # masks = sorted(masks, key=(lambda x: x["area"]), reverse=True)
+        segmentation_results = np.array([item["segmentation"] for item in masks])
+
+        image = image.convert("RGBA")
+        height = segmentation_results[0].shape[0]
+        width = segmentation_results[0].shape[1]
+
+        for i, seg in enumerate(segmentation_results[:20]):
+            mask = np.zeros(
+                (
+                    height,
+                    width,
+                    4,
+                ),
+                dtype=np.uint8,  # important, otherwise there will be errors
             )
-        )
-        overlay[:, :, 3] = 0
-        overlay[ann["segmentation"]] = np.concatenate(
-            [np.random.random(3), [0.35]]
-        )  # 0.35 is opacity
 
-        image.paste(overlay, (0, 0), overlay)
+            mask[seg] = [
+                random.randint(1, 255),
+                random.randint(1, 255),
+                random.randint(1, 255),
+                89,
+            ]
 
-image.save(f"result.png", "PNG")
+            mask = Image.fromarray(mask)
+            if i <= 10:
+                mask.save(f"{output_folder}/{category}_mask_{i}.png", "PNG")
+
+            image.paste(mask, (0, 0), mask)
+
+    image.save(f"{output_folder}/{category}_result.png", "PNG")
